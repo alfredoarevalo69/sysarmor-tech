2dd0fcd0887cc004215dc2bbe8d88922730021db
alfredoarevalo69 <alfredoarevalo69@gmail.com>
2026-08-13 16:49:06 -0500

    publicacion articulo blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sysarmortech.com',
-  output: 'server', // <--- OBLIGATORIO: Habilita Serverless / SSR
+
+  // <--- OBLIGATORIO: Habilita Serverless / SSR
+  output: 'server',
 
   redirects: {
     '/servicios': '/#servicios',
@@ -17,4 +21,5 @@ export default defineConfig({
   },
 
   adapter: vercel(),
+  integrations: [sitemap()],
 });
